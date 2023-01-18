e3f442cd3b3ea877a67587c353ee716312ebdf7b
Ahmed Ramic <ahmedramic@Ahmeds-MacBook-Air.local>
2023-01-17 19:36:10 -0500

    worked on the routes so everything could integrate nicely. The user is for login/logout. The session is for when the user signs in and then can view all the workouts theyve done in the session. The Program routes is for the user to create other programs or to look at the ones theyve already done. The excersice route is the one that collects all the exercises from the ninja api

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,81 +1,33 @@
 const router = require('express').Router();
-const { Workout, User } = require('../models');
+const sequelize = require('../config/connection');
+const User = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all workouts and JOIN with user data
-    const workoutData = await Workout.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
 
-    // Serialize data so the template can read it
-    const workouts = workoutData.map((workout) => workout.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      workouts, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', (req, res) => {
+  res.render('homepage',
+  {
+    logged_in: req.session.logged_in,
+  });
 });
 
-router.get('/workout/:id', async (req, res) => {
-  try {
-    const workoutData = await Workout.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const workout = workoutData.get({ plain: true });
-
-    res.render('workout', {
-      ...workout,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/calendar', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Workout }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('calendar', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+// Requires login to use exercise
+router.get('/exercise', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/calendar');
+    res.render('exercise', {
+      logged_in: req.session.logged_in,
+    });
     return;
   }
+  res.redirect('/login');
+});
 
+// Returns user to homepage if they were already logged in
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 
