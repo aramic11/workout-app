d4a78ef2ef2eda298d5ffc99f51aa482b7a98340
Noah Cote <noahcote10@gmail.com>
2023-01-22 17:43:47 -0500

    removed unnecessary files from directory

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,14 +2,10 @@ const sequelize = require('../config/connection');
 const { User, SessionWorkouts, Program, ProgramWorkouts, Session } = require('../models');
 
 const userData = require('./userData.json');
-const workoutData = require('./workoutData.json');
-const categoryData = require('./categoryData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Workout.bulkCreate(workoutData);
-  await Category.bulkCreate(categoryData);
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
