3324f5a5d9c5eedab96c7a4f92b8e9e629ce60a4
Mitchel B <staysaucy555@gmail.com>
2023-01-22 18:46:48 -0500

    Added Calculator route.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
+const calculatorRoutes=require('./calculatorRoutes');
 const calendarRoutes = require('./calendarRoutes');
 const sessionRoutes = require('./sessionRoutes');
 const userRoutes = require("./userRoutes");
 const exerciseRoutes = require('./exerciseRoutes');
 const programRoutes = require('./programRoutes');
 
+router.use('/calculator', calculatorRoutes);
 router.use('/calendar', calendarRoutes);
 router.use('/session', sessionRoutes);
 router.use("/users", userRoutes);
