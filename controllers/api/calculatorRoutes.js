99221f3c55d310020d4b26da7f83677026031e56
Mitchel B <staysaucy555@gmail.com>
2023-01-23 18:30:35 -0500

    FINISHED CALCULATOR

--- controllers/api/calculatorRoutes.js
+++ controllers/api/calculatorRoutes.js
@@ -2,17 +2,4 @@ const router = require('express').Router();
 const { SessionWorkouts } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-      const calendarData = await SessionWorkouts.findAll({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(calendarData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-});
-
 module.exports = router;
